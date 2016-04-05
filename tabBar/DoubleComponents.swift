16426cf824f703874d2f45c258336233fb25abde
Mac <Mac@Macs-Mac.local>
2016-04-05 00:32:43 -0700

    complete double picker componenet

--- tabBar/DoubleComponents.swift
+++ tabBar/DoubleComponents.swift
@@ -12,60 +12,93 @@ class DoubleComponents: UIViewController, UIPickerViewDelegate, UIPickerViewData
 {
     
     @IBOutlet weak var double: UIPickerView!
-
+    
     @IBOutlet weak var txtViewCountryOutlet: UITextField!
     @IBOutlet weak var doubleComponentsPickrOulet: UIPickerView!
-   
+    
     @IBOutlet weak var txtViewCapitalOutlet: UITextField!
     @IBOutlet weak var btnSubmitOutlet: UIButton!
-
-    var pickerViewArray = ["Afganistan", "West Indies", "India", "Australia", "Pakistan", "Sri Lanka", "Bangladesh","South Africa" ]
-    var pickerViewArray2 = [" Kabul ", "Jamaica", "Mumbai", "Melbourn", "Islamabad", "Colombo", "Dhaka","Cape Town" ]
     
-    override func viewDidLoad() {
+    var country = ["Afganistan", "West Indies", "India", "Australia", "Pakistan", "Sri Lanka", "Bangladesh","South Africa" ]
+    var capital = [" Kabul ", "Jamaica", "Mumbai", "Melbourn", "Islamabad", "Colombo", "Dhaka","Cape Town" ]
+    
+    override func viewDidLoad()
+        
+    {
         super.viewDidLoad()
         self.btnSubmitOutlet.layer.cornerRadius = 5
-
-            }
-
-    override func didReceiveMemoryWarning() {
+        
+    }
+    
+    override func didReceiveMemoryWarning()
+        
+    {
         super.didReceiveMemoryWarning()
-       
+        
     }
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return pickerViewArray.count
+        return country.count
     }
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 2
     }
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-       if component == 0
-       {
-        return pickerViewArray[row]
-        }
-       else{
-     return pickerViewArray2[row]
-        }
-    }
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         if component == 0
         {
-        
-        self.txtViewCountryOutlet.text = pickerViewArray[doubleComponentsPickrOulet.selectedRowInComponent(0)]
+            return country[row]
         }
-        else if component == 1
+        else{
+            return capital[row]
+        }
+    }
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+        
+    {
+        if component == 0
+            
         {
-        self.txtViewCapitalOutlet.text = pickerViewArray2[doubleComponentsPickrOulet.selectedRowInComponent(1)]
+            
+            self.txtViewCountryOutlet.text = country[doubleComponentsPickrOulet.selectedRowInComponent(0)]
+            
+        }
+            
+        else if component == 1
+            
+            
+        {
+            
+            self.txtViewCapitalOutlet.text = capital[doubleComponentsPickrOulet.selectedRowInComponent(1)]
         }
     }
     
     @IBAction func btnSubmitAction(sender: AnyObject)
-    
+        
     {
         if txtViewCapitalOutlet != "" && txtViewCountryOutlet != ""
             
         {
-         
+            
+            let index1 = country.indexOf(txtViewCountryOutlet.text!)
+            let index2 = capital.indexOf(txtViewCapitalOutlet.text!)
+            if index1 == index2
+            {
+                let alert = UIAlertController(title: "Your Answer is ", message: "Right", preferredStyle: .Alert)
+                let action = UIAlertAction(title: "Next", style: .Default, handler:nil)
+                alert.addAction(action)
+                presentViewController(alert, animated: true, completion: nil)
+                
+            }
+            else
+            {
+                let alert = UIAlertController(title: "Your Answer is ", message: "Wrong", preferredStyle: .Alert)
+                let action = UIAlertAction(title: "Try Again", style: .Default, handler:nil)
+                alert.addAction(action)
+                presentViewController(alert, animated: true, completion: nil)
+
+                
+            }
+            
+
         }
         
     }
